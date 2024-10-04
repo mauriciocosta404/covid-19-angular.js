b1f3a79692a94297ce170b0fc5b7f8229fc0fc1f
mauriciocosta404 <mc611641@gmail.com>
2024-10-04 16:48:26 +0100

    feat: adicionando o about

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { BannerComponent } from "../../components/banner/banner.component";
 import { CasesComponent } from "../../components/cases/cases.component";
+import { AboutComponent } from "../../components/about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, CasesComponent],
+  imports: [HeaderComponent, BannerComponent, CasesComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
