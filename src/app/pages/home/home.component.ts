3666635775d35b3baafe08b97b977e8cc118006d
mcosta <mauricio.costa@xdsoftware.ao>
2024-10-23 17:57:47 +0100

    feat: adicionando o componente card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { BannerComponent } from "../../components/banner/banner.component";
 import { CasesComponent } from "../../components/cases/cases.component";
 import { AboutComponent } from "../../components/about/about.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, CasesComponent, AboutComponent, FooterComponent],
+  imports: [HeaderComponent, BannerComponent, CasesComponent, AboutComponent, FooterComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
